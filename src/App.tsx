d9ad410b760a744ea92ef959a70f9079636da29e
litc0de <litc0de@posteo.org>
2024-09-23 12:13:20 +0200

    initial commit

---
+++ src/App.tsx
@@ -0,0 +1,13 @@
+import "./App.css";
+
+function App() {
+  return (
+    <>
+      <h1>Hello there 👋</h1>
+      <p>Check out my GitHub:</p>
+      <a href="https://github.com/bastipnt">https://github.com/bastipnt</a>
+    </>
+  );
+}
+
+export default App;
