aaeecd9b79fee1e30fd8a285b21654cc0768542a
litc0de <litc0de@posteo.org>
2024-12-14 18:32:13 +0100

    update stuff

--- src/App.tsx
+++ src/App.tsx
@@ -4,8 +4,20 @@ function App() {
   return (
     <>
       <h1>Hello there 👋</h1>
-      <p>Check out my GitHub:</p>
-      <a href="https://github.com/bastipnt">https://github.com/bastipnt</a>
+      <p>Welcome to my homepage.</p>
+      <p>This is still a work in progress and will be updated in the future.</p>
+      <p>
+        In the meanwhile go and check out my GitHub:{" "}
+        <a href="https://github.com/bastipnt">https://github.com/bastipnt</a>
+      </p>
+      <p>
+        ✨ And if you want you can take part in this survey about advertisement
+        and tracking:{" "}
+        <a href="https://tracking-survey.bastipnt.de">
+          https://tracking-survey.bastipnt.de
+        </a>{" "}
+        ✨
+      </p>
     </>
   );
 }
